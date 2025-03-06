94c7f0ec6990ff38a32383219c9a8d883e6f99d9
xkylz1 <fauzansaputra741@gmail.com>
2025-03-06 11:23:54 +0800

    feat: add landing page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="bg-white shadow-md p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="{{ url('/') }}" class="text-lg font-bold">MyWebsite</a>
+        <ul class="flex space-x-4">
+            <li><a href="#hero" class="hover:text-blue-500">Home</a></li>
+            <li><a href="#insight" class="hover:text-blue-500">Insights</a></li>
+            <li><a href="/login" class="hover:text-blue-500">Masuk</a></li>
+        </ul>
+    </div>
+</nav>
